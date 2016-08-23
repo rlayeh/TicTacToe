f9ec3cf5e522dd46b1fa9b30f533afac411e73a3
unknown <ryszard.jakielski@ryszardj-pc.office.eventis.nl>
2016-08-23 06:35:56 +0200

    Added unit tests for Victory resolver

---
+++ test/engine/victoryResolver.spec.js
@@ -0,0 +1,319 @@
+import expect from 'expect'
+import 'babel-polyfill'
+import victoryResolver from '../../src/engine/victoryResolver'
+
+describe('victoryResolver', () => {
+  it('should exist', () => {
+    expect(victoryResolver).toBeTruthy()
+  })
+
+  it('should return null for boardSize equal 0',()=>{
+    expect(victoryResolver([{},{}],0)).toBe(null)
+  })
+
+  it('should return null for empty moves array',()=>{
+    expect(victoryResolver([],2)).toBe(null)
+  })
+
+  it('should return winner for first row', ()=>{
+    const moves = [{
+      x: 0,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 2,
+      type: 'cross'
+    },
+    {
+      x: 2,
+      y: 0,
+      type: 'circle'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('circle')
+  })
+
+  it('should return winner for second row', ()=>{
+    const moves = [{
+      x: 0,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 1,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 0,
+      y: 2,
+      type: 'circle'
+    },
+    {
+      x: 2,
+      y: 1,
+      type: 'cross'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('cross')
+  })
+
+  it('should return winner for third row', ()=>{
+    const moves = [{
+      x: 0,
+      y: 2,
+      type: 'circle'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 2,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 2,
+      y: 2,
+      type: 'circle'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('circle')
+  })
+
+  it('should return winner for first column', ()=>{
+    const moves = [{
+      x: 0,
+      y: 0,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 2,
+      y: 1,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 2,
+      type: 'cross'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('cross')
+  })
+
+  it('should return winner for second column', ()=>{
+    const moves = [{
+      x: 1,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 2,
+      y: 0,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 1,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 2,
+      type: 'circle'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('circle')
+  })
+
+  it('should return winner for third column', ()=>{
+    const moves = [{
+      x: 2,
+      y: 0,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 2,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'circle'
+    },
+    {
+      x: 2,
+      y: 2,
+      type: 'cross'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('cross')
+  })
+
+
+  it('should return winner for diagonal top left to bottom right', ()=>{
+    const moves = [{
+      x: 0,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 1,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 2,
+      y: 2,
+      type: 'circle'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('circle')
+  })
+
+
+  it('should return winner for diagonal bottom left to top right', ()=>{
+    const moves = [{
+      x: 0,
+      y: 2,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 1,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'circle'
+    },
+    {
+      x: 2,
+      y: 0,
+      type: 'cross'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe('cross')
+  })
+
+  it('should not return a winner if there are less than minimal amount of moves', ()=>{
+    const moves = [{
+      x: 0,
+      y: 0,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 1,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 2,
+      y: 2,
+      type: 'cross'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe(null)
+  })
+
+  it('should return null for not winning scenario', ()=>{
+    const moves = [{
+      x: 0,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 1,
+      y: 0,
+      type: 'cross'
+    },
+    {
+      x: 2,
+      y: 0,
+      type: 'circle'
+    },
+    {
+      x: 0,
+      y: 1,
+      type: 'cross'
+    },
+    {
+      x: 1,
+      y: 1,
+      type: 'circle'
+    },
+    {
+      x: 2,
+      y: 1,
+      type: 'cross'
+    }]
+
+    expect(victoryResolver(moves,3)).toBe(null)
+  })
+})
